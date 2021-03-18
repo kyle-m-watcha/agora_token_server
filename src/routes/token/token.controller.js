9f8720bea3a7c57b5eaf654372439f6de515b1a8
kyle-moon <kyle.m@watcha.com>
2021-03-18 18:02:57 +0900

    feat: 토큰 업데이트

--- src/routes/token/token.controller.js
+++ src/routes/token/token.controller.js
@@ -24,7 +24,7 @@ class TokenController {
     res.json({
       ok: true,
       token:
-        "006967c262fe6b8417bab8058645362e2daIADZuWvuPg8ZATdnnN/+9KeAXmjnYBFSc/7+sflrMNhwhdfkF4QAAAAAEAC5X9YGyOJSYAEAAQDH4lJg",
+        "006967c262fe6b8417bab8058645362e2daIAD22r0ESNzZ0dTMsyLbCm6O9m5/PhW2Sz+nEpRuj6K2ddfkF4QAAAAAEAC5X9YGgWhUYAEAAQCBaFRg",
     });
   };
 }
