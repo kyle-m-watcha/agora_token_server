6458d4d0f051a2e796d3d1001acca83c6d93ca2a
kyle-moon <kyle.m@watcha.com>
2021-03-23 18:26:59 +0900

    Update token

--- src/routes/token/token.controller.js
+++ src/routes/token/token.controller.js
@@ -24,7 +24,7 @@ class TokenController {
     res.json({
       ok: true,
       token:
-        "006967c262fe6b8417bab8058645362e2daIABhWSxDPre0o0YYcX+DSA67kceZQN51dMngZBipBVV8NtfkF4QAAAAAEAC5X9YGerNaYAEAAQB6s1pg",
+        "006967c262fe6b8417bab8058645362e2daIACq/jByJI2leeZ6Saro5J8NoxuGrePRG9YnWj4B55D739fkF4QAAAAAEAC5X9YG1wVbYAEAAQDXBVtg",
     });
   };
 }
